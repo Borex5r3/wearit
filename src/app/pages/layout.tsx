ec1ef9c47067db8c9d9bf476416745b9d7d6ce67
Abdellah Dbaich <adbaich@e2r12p1.1337.ma>
2024-02-27 11:22:42 +0100

    end of navbar desktop version

--- src/app/pages/layout.tsx
+++ src/app/pages/layout.tsx
@@ -5,25 +5,14 @@ import NavBarDesktop from "./navBarDesktop";
 import { useState } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const [mouseClickPosition, setMouseClickPosition] = useState({ x: 0, y: 0 });
   const [showProfileBar, setShowProfileBar] = useState(false);
 
-  const handleMouseClick = (e: any) => {
-    if (showProfileBar) {
-      const obj = {
-        x: e.clientX,
-        y: e.clientY,
-      };
-      setMouseClickPosition(obj);
-    }
-  };
   return (
-    <div onClick={handleMouseClick} className="flex h-screen flex-col">
+    <div className="flex h-screen flex-col">
       {/* <NavBarMobile /> */}
       <NavBarDesktop
         showProfileBar={showProfileBar}
         setShowProfileBar={setShowProfileBar}
-        mouseClickPosition={mouseClickPosition}
       />
       <div className="flex-grow md:overflow-y-auto">
         {children}
--- src/app/pages/navBarDesktop.tsx
+++ src/app/pages/navBarDesktop.tsx
@@ -5,25 +5,13 @@ import { useRef } from "react";
 export default function ({
   showProfileBar,
   setShowProfileBar,
-  mouseClickPosition,
 }: {
   showProfileBar: boolean;
   setShowProfileBar: any;
-  mouseClickPosition: any;
 }) {
-  const profileBarRef = useRef();
   const print = (e: any) => {
     console.log(e.clientX);
   };
-  if (showProfileBar) {
-    const divX = profileBarRef.current?.offsetLeft;
-    const divY = profileBarRef.current?.offsetTop;
-    const { x, y } = mouseClickPosition;
-    if (!(divX >= x && divX + 280 < x && divY >= y && divY + 105 < y)) {
-      setShowProfileBar(!showProfileBar);
-    }
-    console.log(divX, divY);
-  }
   return (
     <div className="min-w-[280px] bg-eggplant">
       <div className="flex justify-between px-[200px] py-[16px] items-center h-full">
@@ -70,9 +58,9 @@ export default function ({
             className={`absolute z-50 pl-[40px] py-[20px] flex flex-col w-[280px] h-[105px] justify-center items-center top-[64px] left-[-300px] bg-[white] duration-200 ${
               showProfileBar ? " scale-y-100" : "scale-y-0"
             } origin-top ease-in-out text-black space-y-[24px]`}
-            ref={profileBarRef}
           >
             <Link
+              onClick={() => setShowProfileBar(!showProfileBar)}
               href={"/pages/cart"}
               className="flex space-x-[28px] items-center w-full"
             >
@@ -85,6 +73,7 @@ export default function ({
               <p className="text-[14px] font-medium antialiased">My Cart</p>
             </Link>
             <Link
+              onClick={() => setShowProfileBar(!showProfileBar)}
               href={"/pages/sign-in"}
               className="flex space-x-[28px] items-center w-full"
             >
