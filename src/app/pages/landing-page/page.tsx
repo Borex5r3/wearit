c492cbb3f121c9f215a1c0d01ab2a7101e55d5a4
Abdellah Dbaich <adbaich@e2r13p2.1337.ma>
2024-03-01 17:25:22 +0100

    adding the width of screen shared between all the pages components

--- src/app/pages/landing-page/page.tsx
+++ src/app/pages/landing-page/page.tsx
@@ -1,11 +1,15 @@
 "use client";
-import LandingPageDesktop from './desktop/landing-page-desktop';
-import LandingPageMobile from './mobile/landing-page-mobile';
+import { useContext } from "react";
+import LandingPageDesktop from "./desktop/landing-page-desktop";
+import LandingPageMobile from "./mobile/landing-page-mobile";
+import { BreakpointContext } from "../layout";
 
 export default function () {
+  const { isBreakpoint } = useContext(BreakpointContext);
 
   return (
-    // <LandingPageMobile />
-    <LandingPageDesktop />
+    <>{isBreakpoint ? <LandingPageMobile /> : <LandingPageDesktop />}</>
+    // // <LandingPageMobile />
+    // <LandingPageDesktop />
   );
 }
--- src/app/pages/layout.tsx
+++ src/app/pages/layout.tsx
@@ -2,24 +2,63 @@
 import Footer from "@/app/pages/footer";
 import NavBarMobile from "@/app/pages/navBarMobile";
 import NavBarDesktop from "./navBarDesktop";
-import { useState } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import FooterDesktop from "./footerDesktop";
+
+const useMediaQuery = (width: any) => {
+  const [targetReached, setTargetReached] = useState(false);
+
+  const updateTarget = useCallback((e: any) => {
+    if (e.matches) {
+      setTargetReached(true);
+    } else {
+      setTargetReached(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    const media = window.matchMedia(`(max-width: ${width}px)`);
+    media.addListener(updateTarget);
+
+    // Check on mount (callback is not called until a change occurs)
+    if (media.matches) {
+      setTargetReached(true);
+    }
+
+    return () => media.removeListener(updateTarget);
+  }, []);
+
+  return targetReached;
+};
+export const BreakpointContext = createContext({ isBreakpoint: false });
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const [showProfileBar, setShowProfileBar] = useState(false);
+  const isBreakpoint = useMediaQuery(1366);
+  console.log(isBreakpoint);
 
   return (
-    <div className="flex h-screen flex-col">
-      {/* <NavBarMobile /> */}
-      <NavBarDesktop
-        showProfileBar={showProfileBar}
-        setShowProfileBar={setShowProfileBar}
-      />
-      <div className="flex-grow md:overflow-y-auto">
-        {children}
-        {/* <Footer /> */}
-        <FooterDesktop />
+    <BreakpointContext.Provider value={{ isBreakpoint: isBreakpoint }}>
+      <div className="flex h-screen flex-col">
+        {isBreakpoint ? (
+          <NavBarMobile />
+        ) : (
+          <NavBarDesktop
+            showProfileBar={showProfileBar}
+            setShowProfileBar={setShowProfileBar}
+          />
+        )}
+        <div className="flex-grow md:overflow-y-auto">
+          {children}
+          {isBreakpoint ? <Footer /> : <FooterDesktop />}
+        </div>
       </div>
-    </div>
+    </BreakpointContext.Provider>
   );
 }
