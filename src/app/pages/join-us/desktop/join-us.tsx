d124777a45f5e9155ca1ae89ddb0ab6267417c4a
Abdellah Dbaich <adbaich@e2r12p1.1337.ma>
2024-03-02 18:53:40 +0100

    adding join us version desktop

---
+++ src/app/pages/join-us/desktop/join-us.tsx
@@ -0,0 +1,67 @@
+import { ToastContainer } from "react-toastify";
+import Link from "next/link";
+import InputContainer from "../../../components/inputContainer";
+
+export default function ({
+  handleSubmit,
+  onSubmit,
+  joinUsInputs,
+  register,
+  errors,
+}: {
+  handleSubmit: any;
+  onSubmit: any;
+  joinUsInputs: any;
+  register: any;
+  errors: any;
+}) {
+  return (
+    <div className="bg-eggplant text-greypayne flex flex-col items-center pb-[100px] pt-[40px] space-y-[40px]">
+      <p className="text-white text-[100px] font-bold antialiased">WearIt</p>
+      <div className="flex flex-col max-w-[800px] w-full border px-[36px] space-y-[76px] bg-white pb-[200px] pt-[24px]">
+        <div className="w-full flex">
+          <Link
+            href={"/pages/sign-in"}
+            className="font-bold text-[16px] antialiased uppercase text-center border-b-[4px] border-greypayne w-full py-[16px]"
+          >
+            join us
+          </Link>
+          <Link
+            href={"/pages/sign-in"}
+            className="font-medium text-[16px] antialiased uppercase text-center w-full py-[16px]"
+          >
+            sign in
+          </Link>
+        </div>
+        {/* form */}
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="w-full flex flex-col items-center space-y-[32px]"
+        >
+          <div className="w-full px-[20px] space-y-[20px] flex flex-col items-center max-w-[525px]">
+            {joinUsInputs.map((item: any) => {
+              return (
+                <InputContainer
+                  key={item.id}
+                  item={item}
+                  register={register}
+                  errors={errors}
+                />
+              );
+            })}
+          </div>
+          {/* button */}
+          <div className="w-full px-[20px] flex flex-col items-center max-w-[525px]">
+            <button
+              type="submit"
+              className=" uppercase text-[16px] font-bold antialiased bg-eggplant text-white py-[24px] w-full"
+            >
+              join us
+            </button>
+          </div>
+        </form>
+      </div>
+      <ToastContainer />
+    </div>
+  );
+}
--- src/app/pages/join-us/page.tsx
+++ src/app/pages/join-us/page.tsx
@@ -3,10 +3,14 @@ import InputContainer from "../../components/inputContainer";
 import Link from "next/link";
 import { joinUsInputs } from "../data";
 import { useForm } from "react-hook-form";
-import { useRef } from "react";
+import { useContext, useRef } from "react";
 import { ToastContainer, toast } from "react-toastify";
+import JoinUsMobile from "./mobile/join-us";
+import JoinUsDesktop from "./desktop/join-us";
+import { BreakpointContext } from "../layout";
 
 export default function () {
+  const { isBreakpoint } = useContext(BreakpointContext);
   const {
     register,
     handleSubmit,
@@ -32,50 +36,21 @@ export default function () {
     toastMessage();
     reset();
   };
-  return (
-    <div className="bg-white text-greypayne flex flex-col items-center space-y-[32px] min-w-[280px] pb-[48px] min-h-[800px]">
-      <div className="w-full flex flex-col">
-        <Link
-          href={"/pages/sign-in"}
-          className="font-bold text-[15px] antialiased uppercase text-center border-b-[4px] border-greypayne w-full py-[16px]"
-        >
-          join us
-        </Link>
-        <Link
-          href={"/pages/sign-in"}
-          className="font-medium text-[15px] antialiased uppercase text-center border-b border-greypayne w-full py-[16px]"
-        >
-          sign in
-        </Link>
-      </div>
-      {/* form */}
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="w-full flex flex-col items-center space-y-[32px]"
-      >
-        <div className="w-full px-[20px] space-y-[20px] flex flex-col items-center max-w-[525px]">
-          {joinUsInputs.map((item) => {
-            return (
-              <InputContainer
-                key={item.id}
-                item={item}
-                register={register}
-                errors={errors}
-              />
-            );
-          })}
-        </div>
-        {/* button */}
-        <div className="w-full px-[20px] flex flex-col items-center max-w-[525px]">
-          <button
-            type="submit"
-            className=" uppercase text-[15px] font-bold antialiased bg-eggplant text-white py-[24px] w-full"
-          >
-            join us
-          </button>
-        </div>
-      </form>
-      <ToastContainer />
-    </div>
+  return isBreakpoint ? (
+    <JoinUsMobile
+      handleSubmit={handleSubmit}
+      onSubmit={onSubmit}
+      joinUsInputs={joinUsInputs}
+      register={register}
+      errors={errors}
+    />
+  ) : (
+    <JoinUsDesktop
+      handleSubmit={handleSubmit}
+      onSubmit={onSubmit}
+      joinUsInputs={joinUsInputs}
+      register={register}
+      errors={errors}
+    />
   );
 }
